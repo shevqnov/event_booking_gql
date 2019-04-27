cb25ca062077ccc9626909573e27b4f1ebff396c
shevqnov <sshevqnov@yandex.ru>
2019-04-28 02:15:39 +0500

    added users

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const Event = require('./models/event')
+const User = require('./models/user')
 
 const app = express()
 
@@ -22,6 +24,17 @@ app.use(
         date: String!
       }
 
+      type User {
+        _id: ID!
+        email: String!
+        password: String
+      }
+
+      input UserInput {
+        email: String!
+        password: String!
+      }
+
       input EventInput {
         title: String!
         description: String!
@@ -35,6 +48,7 @@ app.use(
 
       type RootMutation {
          createEvent(eventInput: EventInput!): Event
+         createUser(userInput: UserInput!): User
       }
 
       schema {
@@ -60,7 +74,22 @@ app.use(
             // eslint-disable-next-line no-console
             .catch(err => console.error(err))
         )
-      }
+      },
+      createUser: ({ userInput: { email, password } }) =>
+        User.findOne({ email })
+          .then(user => {
+            if (user) throw new Error('User alredy exists')
+            return bcrypt.hash(password, 12)
+          })
+          .then(password =>
+            new User({
+              email,
+              password
+            }).save()
+          )
+          .then(user => ({ ...user._doc, _id: user.id }))
+          // eslint-disable-next-line no-console
+          .catch(err => console.error(err))
     },
     graphiql: true
   })
