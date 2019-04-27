e962ff49e90d5741eea4f677289029b0e72616a7
shevqnov <sshevqnov@yandex.ru>
2019-04-27 23:45:34 +0500

    dynamic create events

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const nanoid = require('nanoid')
 
 const app = express()
+
+const events = []
 
 app.use(bodyParser.json())
 
@@ -11,12 +14,27 @@ app.use(
   '/graphql',
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
       type RootQuery {
-         events: [String!]!
+         events: [Event!]!
       }
 
       type RootMutation {
-         createEvent(name: String): String
+         createEvent(eventInput: EventInput!): Event
       }
 
       schema {
@@ -26,11 +44,15 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ['Cooking', 'Beer Drinking', 'Sailing our souls', 'Midnight coding']
+        return events
       },
-      createEvent: args => {
-        const eventName = args.name
-        return eventName
+      createEvent: ({ eventInput }) => {
+        const event = {
+          _id: nanoid(),
+          ...eventInput
+        }
+        events.push(event)
+        return event
       }
     },
     graphiql: true
