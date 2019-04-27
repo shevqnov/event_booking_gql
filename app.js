be920078ff5cdc2340b8524daa37790442ff145d
shevqnov <sshevqnov@yandex.ru>
2019-04-28 01:15:39 +0500

    mongoose implemented

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
-const nanoid = require('nanoid')
+const mongoose = require('mongoose')
+
+const Event = require('./models/event')
 
 const app = express()
-
-const events = []
 
 app.use(bodyParser.json())
 
@@ -43,20 +43,34 @@ app.use(
       }
     `),
     rootValue: {
-      events: () => {
-        return events
-      },
+      events: () =>
+        Event.find()
+          .then(events => events.map(({ _doc: event }) => ({ ...event, _id: event._id.toString() })))
+          // eslint-disable-next-line no-console
+          .catch(err => console.error(err)),
       createEvent: ({ eventInput }) => {
-        const event = {
-          _id: nanoid(),
-          ...eventInput
-        }
-        events.push(event)
-        return event
+        const event = new Event({
+          ...eventInput,
+          date: new Date(eventInput.date)
+        })
+        return (
+          event
+            .save()
+            .then(({ _doc: event }) => ({ ...event, _id: event._id.toString() }))
+            // eslint-disable-next-line no-console
+            .catch(err => console.error(err))
+        )
       }
     },
     graphiql: true
   })
 )
-
-app.listen(3000)
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-dngap.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true`
+  )
+  .then(app.listen(3000))
+  // eslint-disable-next-line no-console
+  .catch(err => console.error(err))
